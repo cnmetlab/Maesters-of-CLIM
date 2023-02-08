ddc4b6ac205f086573a9a14c0b7aaa13e3189a91
blizhan <blizhan@mail.nankai.edu.cn>
2023-02-08 08:52:00 +0800

    add: pypi publish

--- readme.md
+++ readme.md
@@ -4,9 +4,15 @@
 
 Maesters-of-Clim tempt to help retriving climate data (climate index, reanalysis) from the main-stream climate insitution (like IRI, PSL, NCEI, RDA). 
 
+### Install
+```shell
+pip install maesters_of_clim==0.0.1
+```
+
 ### Usage
 ```python
 from maesters_of_clim import Climate_Maester
+from datetime import datetime
 
 # retrive history climate index from nina
 c = Climate_Maester(
@@ -24,7 +30,7 @@ iridf = c.forecast(pred_at=datetime(2022, 10, 1))
 
 c = Climate_Maester(
     indexes='enso',
-    source'cpc'
+    source='cpc'
 )
 cpcdf = c.forecast(pred_at=datetime(2022, 10, 1))
 ```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(FILE_PATH, "README.md"), "r") as f:
+    description = f.read()
+
+with open(os.path.join(FILE_PATH, "requirements.txt")) as f:
+    required = f.read().splitlines()
+
+setup(
+    name="maesters-clim",
+    version="0.0.1",
+    author="blizhan",
+    author_email="blizhan@icloud.com",
+    description="Maesters-of-Clim tempt to help retriving climate data (climate index, reanalysis) from the main-stream climate insitution (like IRI, PSL, NCEI, RDA).",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blizhan/maesters-of-clim",
+    package_dir={"maesters-of-clim": "maesters-of-clim", ".": "./"},
+    package_data={
+        "": ["*.toml", "*.txt"],
+    },
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
